d1fb169bcf81aa5d27d520f03727ae22856894d2
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2023-04-10 03:49:10 +0200

    adding jobs page

--- assets/js/pages/area-filter-range.init.js
+++ assets/js/pages/area-filter-range.init.js
@@ -1,1 +1,10 @@
-var slider1=document.getElementById("slider1");noUiSlider.create(slider1,{start:[9],step:1,range:{min:[1],max:[15]}});var slider1Value=document.getElementById("slider1-span");slider1.noUiSlider.on("update",function(e,r){slider1Value.innerHTML=e[r]});
+var slider1 = document.getElementById("slider1");
+noUiSlider.create(slider1, {
+  start: [200],
+  step: 5,
+  range: { min: [200], max: [100000] },
+});
+var slider1Value = document.getElementById("slider1-span");
+slider1.noUiSlider.on("update", function (e, r) {
+  slider1Value.innerHTML = e[r];
+});
