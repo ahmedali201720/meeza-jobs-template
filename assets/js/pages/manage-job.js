a11249dbaff62752fe35dbe05516feff2b5f9474
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2023-04-26 01:57:58 +0200

    adding new post job page

---
+++ assets/js/pages/manage-job.js
@@ -0,0 +1,27 @@
+function pageReady() {
+  // Choice Js Elements
+  const categoriesElement = document.getElementById("categories");
+  const typeElement = document.getElementById("type");
+  const skillElement = document.getElementById("skills");
+  let categoryChoice = null;
+  let typeChoice = null;
+  let skillChoice = null;
+  if (categoriesElement) {
+    categoryChoice = new Choices(categoriesElement);
+  }
+  if (typeElement) {
+    typeChoice = new Choices(typeElement);
+  }
+  if (skillElement) {
+    skillChoice = new Choices(skillElement);
+  }
+  // Date Ranger Picker
+  $(".rangeinput").daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    minYear: 1970,
+    maxYear: parseInt(moment().format("YYYY"), 10),
+  });
+}
+
+document.addEventListener("DOMContentLoaded", pageReady, false);
