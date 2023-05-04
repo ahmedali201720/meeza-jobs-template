16770fb7ce3399989c2d5566280ba1d96dc0feb7
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2023-05-04 05:06:30 +0300

    working on home page wuzzuf version

--- assets/js/pages/job-list.init.js
+++ assets/js/pages/job-list.init.js
@@ -1,1 +1,19 @@
-var singleCategories,singleLocation=new Choices("#choices-single-location"),singleCategorie=document.getElementById("choices-single-categories");singleCategorie&&(singleCategories=new Choices("#choices-single-categories"));
+var singleCategories,
+  singleLocation = new Choices("#choices-single-location"),
+  singleCategorie = document.getElementById("choices-single-categories");
+singleCategorie &&
+  (singleCategories = new Choices("#choices-single-categories"));
+
+const sectorElement = document.getElementById("choices-sector");
+let sectorSelect = null;
+
+const specialistElement = document.getElementById("choices-specialist");
+let specialistSelect = null;
+
+if (sectorElement) {
+  sectorSelect = new Choices(sectorElement);
+}
+
+if (specialistElement) {
+  specialistSelect = new Choices(specialistElement);
+}
